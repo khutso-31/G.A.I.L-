0ec98db63eabb2cea68a878d75c46b389223da8b
khutso-31 <202344566@spu.ac.za>
2025-07-10 13:12:55 +0200

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,1 +1,16 @@
+import streamlit as st
+
+st.title("🔐 Login")
+
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if username == "test" and password == "1234":
+        st.session_state.logged_in = True
+        st.session_state.username = username
+        st.success("Logged in!")
+        st.switch_page("pages/2_Questionnaire.py")
+    else:
+        st.error("Invalid credentials.")
 
